38e4aeba1186d8cacd0bd7de8c18beee202b978c
cube <icegame1118@gmail.com>
2024-01-06 18:06:03 +0900

    updated main.c

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 #include<stdbool.h>
-#define n 3000
+#define N 3000
 
 int main(void){
-	bool prime[n+1];
-	for(int i=0;i<n;i++)prime[i]=true;
-	for(int p=2;p*p<=n;p++){
+	bool prime[N+1];
+	for(int i=0;i<N;i++)prime[i]=true;
+	for(int p=2;p*p<=N;p++){
 		if(prime[p]==true){
-			for(int i=p*p;i<=n;i+=p)prime[i]=false;
+			for(int i=p*p;i<=N;i+=p)prime[i]=false;
 		}
 	}
 	//output
-	for(int k=2;k<=n;k++){
+	for(int k=2;k<=N;k++){
 		if(prime[k])printf("%d\n",k);
 	}
 	return 0;
